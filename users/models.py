71321f3dc1478750a3d842322fc070690f710c05
João Gomes <joaosilgo96@gmail.com>
2020-10-21 01:19:13 +0100

    🐍 V.10 Setup Deploy

--- users/models.py
+++ users/models.py
@@ -13,9 +13,9 @@ class Profile(models.Model):
     def save(self, **kwargs):
         super().save()
 
-        img = Image.open(self.image.path)
+        #img = Image.open(self.image.path)
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+       # if img.height > 300 or img.width > 300:
+       #     output_size = (300, 300)
+        #    img.thumbnail(output_size)
+        #    img.save(self.image.path)
